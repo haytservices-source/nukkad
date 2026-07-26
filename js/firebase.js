3452c9b3da6ef3bccda0db62429c62d57acf0b6a
haytservices-source <haytservices@gmail.com>
2026-07-26 18:59:28 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,72 +1,52 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
 import {
-    getFirestore,
-    collection,
-    addDoc,
-    getDocs,
-    getDoc,
-    setDoc,
-    updateDoc,
-    deleteDoc,
-    doc,
-    onSnapshot,
-    query,
-    where,
-    orderBy,
-    serverTimestamp
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+  getDatabase,
+  ref,
+  push,
+  set,
+  get,
+  update,
+  remove,
+  onValue
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
 import {
-    getAuth,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 import {
-    getStorage
+  getStorage
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyDSmSlbV9frqwJBN5HLxEsJcRPZRAWuEjM",
-
   authDomain: "zahed-6b455.firebaseapp.com",
-
+  databaseURL: "https://zahed-6b455-default-rtdb.firebaseio.com",
   projectId: "zahed-6b455",
-
   storageBucket: "zahed-6b455.firebasestorage.app",
-
   messagingSenderId: "1083482711534",
-
   appId: "1:1083482711534:web:8a60e365eb65cad7ab9aff"
-
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
-
+export const db = getDatabase(app);
 export const auth = getAuth(app);
-
 export const storage = getStorage(app);
 
 export {
-collection,
-addDoc,
-getDocs,
-getDoc,
-setDoc,
-updateDoc,
-deleteDoc,
-doc,
-query,
-where,
-orderBy,
-onSnapshot,
-serverTimestamp,
-signInWithEmailAndPassword,
-signOut,
-onAuthStateChanged
+  ref,
+  push,
+  set,
+  get,
+  update,
+  remove,
+  onValue,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 };
